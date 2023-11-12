393fd3fe8e489e3fe438ec749312737624e9bac2
rrauf8178 <rrauf8178@gmail.com>
2023-11-13 03:19:26 +0400

    navbar-component/: navbar style and configure

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-export default Navbar
+  return (
+    <div className="navbar">
+      <div className="navbar-content">
+        <div className="container">
+          <div className="navbar-top flex flex-between">
+            <Link to={"/"} className="navbar-brand">
+              <span className="text-regal-blue">Shopping</span>
+              <span className="text-gold">App</span>
+            </Link>
+            <form className="navbar-search flex">
+              <input type="text" placeholder="Search here ..." />
+              <button type="submit">
+                <i className="fas fa-search"></i>
+              </button>
+            </form>
+            <div className="navbar-btns">
+              <Link to={"/cart"} className="add-to-cart-btn flex">
+                <span className="btn-ico">
+                  <i className="fas fa-shopping-cart"></i>
+                </span>
+                <div className="btn-txt fw-5">
+                  cart <span className="cart-count-value">0</span>{" "}
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="navbar-bottom bg-regal-blue">
+          <div className="container flex flex-between">
+            <ul className={`nav-links flex ${isSidebarOpen ? 'show-nav-links' : ""}`}>
+              <button type="button" className="navbar-hide-btn text-white" onClick={() => setIsSidebarOpen(false)}>
+                <i className="fas fa-times"></i>
+              </button>
+              <li>
+                <Link to={"/"} className="nav-link text-white">Demos</Link>
+              </li>
+            </ul>
+            <button type="button" className="navbar-show-btn text-gold" onClick={() => setIsSidebarOpen(true)}>
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
