fca9fa544a05dfcac8f492f71c9b26f2bfef67bb
rrauf8178 <rrauf8178@gmail.com>
2023-11-14 14:29:41 +0400

    navbar-category/: add category to navbar with fetching method and redux-toolkit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Category, Cart } from "./pages/index";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/:id" element={<Category />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/category/:id" element={<Category />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
