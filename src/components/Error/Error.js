f2f8c9a5c3cfa3acfc4b209d33922c141774f212
rrauf8178 <rrauf8178@gmail.com>
2023-11-20 22:38:44 +0400

    error-component/: complate error component

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { error } from "../../utils/images";
+import "./Error.scss";
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <div className="container py-5">
+      <div class="flex flex-center error">
+        <img src={error} alt="error" />
+      </div>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
